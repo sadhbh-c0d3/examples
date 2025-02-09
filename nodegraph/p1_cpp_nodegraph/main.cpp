6b5260fd4f80a7d1c18f4820e56bcdb4a8dab42f
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2025-02-09 21:23:00 +0000

    Added S6, S7 Fork

--- nodegraph/p1_cpp_nodegraph/main.cpp
+++ nodegraph/p1_cpp_nodegraph/main.cpp
@@ -5,6 +5,8 @@ void test_s2_dynamic_graph();
 void test_s3_dynamic_graph_and_pins();
 void test_s4_dynamic_graph_and_shared_ptr();
 void test_s5_dynamic_graph_and_try_connect();
+void test_s6_dynamic_graph_and_fork();
+void test_s7_dynamic_graph_and_fork_fix();
 
 
 int main(int argc, char **argv)
@@ -24,6 +26,12 @@ int main(int argc, char **argv)
 
     // S5: Dyamic graph with dynamic connections
     test_s5_dynamic_graph_and_try_connect();
+
+    // S6: Dynamic graph with forked output
+    test_s6_dynamic_graph_and_fork();
+
+    // S7: Forward propagation fix for forked output
+    test_s7_dynamic_graph_and_fork_fix();
 
     return 0;
 }
